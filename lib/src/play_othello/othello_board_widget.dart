6c52cf45b25db741cbf0cd73aa401802a6c84639
TJ <zigzag37458@gmail.com>
2023-05-17 21:36:08 +0900

    feat: 駒が置ける場所をわかりやすくする。

--- lib/src/play_othello/othello_board_widget.dart
+++ lib/src/play_othello/othello_board_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'othello_board.dart';
 import 'othello_piece.dart';
-import 'othello_rule.dart'; // 追加
+import 'othello_rule.dart';
 
 // オセロの盤面を表示するウィジェット
 class OthelloBoardWidget extends StatelessWidget {
@@ -15,7 +15,7 @@ class OthelloBoardWidget extends StatelessWidget {
   final Color availableColor;
 
   // ルール
-  final OthelloRule rule; // 追加
+  final OthelloRule rule;
 
   // 手番をパラメータとして受け取る
   final Color turn;
@@ -49,21 +49,16 @@ class OthelloBoardWidget extends StatelessWidget {
           OthelloPiece piece = board.pieces[index];
           // 駒を置ける場所かどうか判定する
           bool isAvailable = rule.getAvailablePlaces(turn).contains(piece);
-          // 駒を置ける場所なら背景色をavailableColorにする
-          Color backgroundColor = isAvailable ? availableColor : Colors.transparent;
           // Containerウィジェットで駒を囲む
           return Container(
             // 駒の間に隙間を作るためにマージンを指定する
             margin: EdgeInsets.all(2.0),
-            // 駒の色と形を指定する
+            // 駒の色と形を指定する（colorプロパティも追加する）
             decoration: BoxDecoration(
               color: piece.color,
               shape: BoxShape.circle,
-              // 背景色をdecorationの中に移動する
-              // backgroundBlendMode: BlendMode.multiply, // 背景色と駒の色を重ねるためにブレンドモードを指定する
-              // backgroundColor: backgroundColor,
+              border: isAvailable ? Border.all(color: Colors.yellow, width: 1.0) : null, // ここで駒を置ける場所なら黄色の枠線を描く
             ),
-            // 駒をタップしたときの処理を指定する
             child: InkWell(
               onTap: () => onTap(piece),
             ),
